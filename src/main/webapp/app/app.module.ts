10e9247be43034080903c4b64943f930adb55c7c
Bala <balagangadhar85@gmail.com>
2017-04-08 17:25:24 +0800

    Initial import

---
+++ src/main/webapp/app/app.module.ts
@@ -0,0 +1,55 @@
+import './vendor.ts';
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { Ng2Webstorage } from 'ng2-webstorage';
+
+import { MockupinterviewSharedModule, UserRouteAccessService } from './shared';
+import { MockupinterviewHomeModule } from './home/home.module';
+import { MockupinterviewAdminModule } from './admin/admin.module';
+import { MockupinterviewAccountModule } from './account/account.module';
+import { MockupinterviewEntityModule } from './entities/entity.module';
+
+import { LayoutRoutingModule } from './layouts';
+import { customHttpProvider } from './blocks/interceptor/http.provider';
+import { PaginationConfig } from './blocks/config/uib-pagination.config';
+
+import {
+    JhiMainComponent,
+    NavbarComponent,
+    FooterComponent,
+    ProfileService,
+    PageRibbonComponent,
+    ActiveMenuDirective,
+    ErrorComponent
+} from './layouts';
+
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        LayoutRoutingModule,
+        Ng2Webstorage.forRoot({ prefix: 'jhi', separator: '-'}),
+        MockupinterviewSharedModule,
+        MockupinterviewHomeModule,
+        MockupinterviewAdminModule,
+        MockupinterviewAccountModule,
+        MockupinterviewEntityModule
+    ],
+    declarations: [
+        JhiMainComponent,
+        NavbarComponent,
+        ErrorComponent,
+        PageRibbonComponent,
+        ActiveMenuDirective,
+        FooterComponent
+    ],
+    providers: [
+        ProfileService,
+        customHttpProvider(),
+        PaginationConfig,
+        UserRouteAccessService
+    ],
+    bootstrap: [ JhiMainComponent ]
+})
+export class MockupinterviewAppModule {}
